2b3af8647995c1cef0d00289663cc47e4170ba6e
Alif Andarta Al Falah <alifandartaa@yahoo.com>
2022-09-20 10:59:33 +0700

    tugas 7 install react

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-content">
+        <h1>Hi, Saya Alif Andarta</h1>
+        <p>Saya dari Batch 38 sudah berhasil menginstall React</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
