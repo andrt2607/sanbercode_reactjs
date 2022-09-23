e74f1f535afc396682437ad6461e86e7eac625f5
Alif Andarta Al Falah <alifandartaa@yahoo.com>
2022-09-23 23:39:18 +0700

    tugas 10 tailwind react

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Tugas7 from './tugas7/tugas7';
 import Tugas8 from './tugas8/tugas8';
 import Tugas9 from './tugas9/tugas9';
+import { Navbar } from 'flowbite-react';
+import Tugas10 from './tugas10/tugas10';
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
       <Tugas7/>
       <Tugas8 name="Alif Andarta" batch="38" email="alifandarta26@gmail.com"/>
       <Tugas9/>
+      <Tugas10/>
     </>
   );
 }
---
+++ tugas-reactjs/src/tugas10/tugas10.js
@@ -0,0 +1,135 @@
+import React from 'react';
+
+const Tugas10 = () => {
+    return (
+        <>
+            <div className='flex justify-center'>
+                <div className="overflow-x-auto shadow-md sm:rounded-lg mb-8 w-8/12">
+                    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead className="text-xs text-white uppercase bg-purple-500 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" className="py-3 px-6">
+                                    No
+                                </th>
+                                <th scope="col" className="py-3">
+                                    Nama
+                                </th>
+                                <th scope="col" className="py-3">
+                                    Mata Kuliah
+                                </th>
+                                <th scope="col" className="py-3">
+                                    Nilai
+                                </th>
+                                <th scope="col" className="py-3">
+                                    Index Nilai
+                                </th>
+                                <th scope="col" className="py-3">
+                                    Action
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th scope="row" className="py-1 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    1
+                                </th>
+                                <td className="py-4">
+                                    John
+                                </td>
+                                <td className="py-4">
+                                    Algoritma
+                                </td>
+                                <td className="py-4">
+                                    80
+                                </td>
+                                <td className="py-4">
+                                    A
+                                </td>
+                                <td className="py-4">
+                                    <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Edit</button>
+                                    <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                        <tbody>
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    2
+                                </th>
+                                <td className="py-4">
+                                    Doe
+                                </td>
+                                <td className="py-4">
+                                    Matematika
+                                </td>
+                                <td className="py-4">
+                                    70
+                                </td>
+                                <td className="py-4">
+                                    B
+                                </td>
+                                <td className="py-4">
+                                    <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Edit</button>
+                                    <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                        <tbody>
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    3
+                                </th>
+                                <td className="py-4">
+                                    Frank
+                                </td>
+                                <td className="py-4">
+                                    Kalkulus
+                                </td>
+                                <td className="py-4">
+                                    60
+                                </td>
+                                <td className="py-4">
+                                    C
+                                </td>
+                                <td className="py-4">
+                                    <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Edit</button>
+                                    <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                        <tbody>
+                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    4
+                                </th>
+                                <td className="py-4">
+                                    Jason
+                                </td>
+                                <td className="py-4">
+                                    Basis data
+                                </td>
+                                <td className="py-4">
+                                    90
+                                </td>
+                                <td className="py-4">
+                                    A
+                                </td>
+                                <td className="py-4">
+                                    <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Edit</button>
+                                    <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default Tugas10;
