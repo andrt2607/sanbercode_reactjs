52b7842cf649dc249edb569971b2d4ea03bb189e
Alif Andarta Al Falah <alifandartaa@yahoo.com>
2022-09-28 16:34:39 +0700

    change btn edit color to yellow

--- tugas-reactjs/src/tugas13/tugas13.js
+++ tugas-reactjs/src/tugas13/tugas13.js
@@ -193,7 +193,7 @@ const Tugas13 = () => {
                                                 {handleIndexScore(res.score)}
                                             </td>
                                             <td className="py-4">
-                                                <button onClick={handleEdit} value={res.id} type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Edit</button>
+                                                <button onClick={handleEdit} value={res.id} type="button" className="focus:outline-none text-white bg-yellow-300 hover:bg-yellow-300 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Edit</button>
                                                 <button onClick={handleDelete} value={res.id} type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
                                             </td>
                                         </tr>
